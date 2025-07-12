2af2bd0bd10661de7afb7dd7b730194a42438c68
Hallan Miranda <hallanlmm@gmail.com>
2025-07-12 16:53:59 -0300

    Changing readme file

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+         job job1 = new job();
+        job1._company = "Metanoeo";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        job1.Display();
     }
 }
