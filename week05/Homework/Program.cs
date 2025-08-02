f610e4c32060a4a5ab13c480efb38f35be8a0fce
Hallan Miranda <hallanlmm@gmail.com>
2025-08-01 21:27:29 -0300

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment("John Doe", "Fractions", "7.3", "3-10, 20-21");
+        Console.WriteLine(math.GetSummary());           // John Doe - Fractions
+        Console.WriteLine(math.GetHomeworkList());      // Section 7.3 Problems 3-10, 20-21
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());        // Mary Waters - European History
+        Console.WriteLine(writing.GetWritingInformation()); // The Causes of World War II by Mary Waters
     }
 }
