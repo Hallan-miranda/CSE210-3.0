77ed1908a83068bb1cfcf2ad77eba427d3f5d0a0
Hallan Miranda <hallanlmm@gmail.com>
2025-08-04 17:51:38 -0300

    shapes

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,15 @@
+    public class Circle : Shape
+    {
+
+        public double _radius;
+
+        public Circle(double radius, string color) : base(color)
+        {
+            _radius = radius;
+            _color = color;
+        }
+        public override double GetArea()
+        {
+            return 3.14 * _radius * _radius;
+        }
+    }
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Creating instances of shapes
+        Square square = new Square(4, "Red");
+        Circle circle = new Circle(5, "Blue");
+        Rectangle rectangle = new Rectangle(3, 6, "Green");
+
+        // Testing each shape individually
+        Console.WriteLine("Individual shape tests:");
+        Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea()}");
+        Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        // Creating a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        // Iterating through the list and displaying color and area
+        Console.WriteLine("\nIterating through the list of shapes:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape - Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
